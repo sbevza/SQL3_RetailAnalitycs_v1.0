9d05e7847c6966a7763a31418d6acdee395046a4
Your Name <you@example.com>
2023-10-12 22:37:45 +0300

    edit part4

--- src/part4.sql
+++ src/part4.sql
@@ -19,38 +19,39 @@ AS
 $$
 BEGIN
     -- Общая часть запроса
-    RETURN QUERY (WITH part1 AS (SELECT c.customer_id,
-                                        c.group_id,
-                                        gs.group_name,
-                                        c.group_margin
-                                 FROM calculate_average_margin(
-                                              CASE
-                                                  WHEN calc_method = 1 THEN 5
-                                                  WHEN calc_method = 2 THEN 4
-                                                  ELSE 0
-                                                  END,
-                                              0,
-                                              CASE
-                                                  WHEN calc_method = 2 THEN transaction_count
-                                                  ELSE 0
-                                                  END,
-                                              start_date,
-                                              end_date
-                                          ) c
-                                          LEFT JOIN groups_sku gs ON gs.group_id = c.group_id
-                                 ORDER BY c.customer_id, gs.group_name),
-                       groups AS (SELECT *
-                                  FROM groups
-                                  WHERE group_churn_rate < max_churn_index
-                                    AND group_discount_share < (max_discount_percentage / 100)),
+    RETURN QUERY (WITH aver_mar_calc
+                           AS (SELECT c.customer_id,
+                                      c.group_id,
+                                      gs.group_name,
+                                      c.group_margin
+                               FROM calculate_average_margin(
+                                            CASE
+                                                WHEN calc_method = 1 THEN 5
+                                                WHEN calc_method = 2 THEN 4
+                                                ELSE 0
+                                                END,
+                                            0,
+                                            CASE
+                                                WHEN calc_method = 2 THEN transaction_count
+                                                ELSE 0
+                                                END,
+                                            start_date,
+                                            end_date
+                                        ) c
+                                        LEFT JOIN groups_sku gs ON gs.group_id = c.group_id
+                                        LEFT JOIN groups g on g.customer_id = c.customer_id and g.group_id = c.group_id
+                               WHERE group_churn_rate < max_churn_index
+                                 AND group_discount_share < (max_discount_percentage / 100)
+                               ORDER BY c.customer_id, gs.group_name),
+
                        Required_Check AS (SELECT cards.customer_id,
                                                  ROUND(SUM(t.transaction_summ) / COUNT(t.transaction_id) *
                                                        increase_coeff, 2) AS Required_Check_Measure
                                           FROM cards
                                                    JOIN transactions t ON cards.customer_card_id = t.customer_card_id
-                                          WHERE (calc_method = 1 AND t.transaction_datetime BETWEEN start_date AND end_date)
-                                             OR (calc_method = 2 AND t.transaction_id >=
-                                                                     (SELECT MAX(transaction_id) - transaction_count FROM transactions))
+--                                           WHERE
+--                                               (calc_method = 1 AND t.transaction_datetime BETWEEN start_date AND end_date) OR
+--                                               (calc_method = 2 AND t.transaction_id >= (SELECT MAX(transaction_id) - transaction_count FROM transactions))
                                           GROUP BY cards.customer_id),
 
                        RankedGroups AS (SELECT p1.customer_id,
@@ -59,18 +60,18 @@ BEGIN
                                                CEIL(g.group_minimum_discount / 0.05) * 5                          AS Offer_Discount_Depth,
                                                ROW_NUMBER()
                                                OVER (PARTITION BY p1.customer_id ORDER BY p.group_frequency DESC) AS rn
-                                        FROM part1 p1
+                                        FROM aver_mar_calc p1
                                                  LEFT JOIN groups g ON g.customer_id = p1.customer_id AND g.group_id = p1.group_id
                                                  LEFT JOIN periods p ON p.customer_id = p1.customer_id AND p.group_id = p1.group_id
                                                  LEFT JOIN Required_Check rc ON rc.customer_id = p1.customer_id
                                         WHERE CEIL(g.group_minimum_discount / 0.05) * 5 <=
                                               p1.group_margin * (max_margin_percentage / 100)
                                         ORDER BY customer_id, p.group_frequency DESC)
-                  SELECT RankedGroups.customer_id,
-                         RankedGroups.Required_Check_Measure,
-                         RankedGroups.group_name,
-                         RankedGroups.Offer_Discount_Depth
-                  FROM RankedGroups
+                  SELECT rg.customer_id,
+                         rg.Required_Check_Measure,
+                         rg.group_name,
+                         rg.Offer_Discount_Depth
+                  FROM RankedGroups rg
                   WHERE rn = 1
                   ORDER BY customer_id);
 END;
@@ -92,8 +93,8 @@ FROM form_offer_for_average_check(
 SELECT *
 FROM form_offer_for_average_check(
         calc_method := 1,
-        start_date := '2022-07-01',
-        end_date := '2023-12-31',
+        start_date := '2021-01-01',
+        end_date := '2022-07-31',
         transaction_count := 0,
         increase_coeff := 1.15,
         max_churn_index := 3,
